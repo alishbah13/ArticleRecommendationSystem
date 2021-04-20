d766efac62710fd2eb255c8adc16d4609f274832
alishbah13 <alishbah13@gmail.com>
2021-04-20 12:38:24 +0500

    accounts

--- ARS/ARS/settings.py
+++ ARS/ARS/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'index'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
--- ARS/ARS/urls.py
+++ ARS/ARS/urls.py
@@ -21,7 +21,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('admin/', admin.site.urls),
     path('signup/', views.signup, name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+    
+    
 ]
